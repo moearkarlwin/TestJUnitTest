68a8385b11b926fa3a8b96676c8e941372ce093a
moearkarlwin <moearkarlwin@gmail.com>
2025-11-13 11:42:02 +0630

    Unit Testings updated workflow

--- src/main/java/com/testjuninttest/Main.java
+++ src/main/java/com/testjuninttest/Main.java
@@ -9,4 +9,12 @@ public class Main {
     static void display2(String message) {
         IO.println("Hi " + message);
     }
+
+//    int display_3(int i, int j) {
+//        int result = 0;
+//        try {
+//            result = i / j;
+//        }
+//        catch ()
+//    }
 }
